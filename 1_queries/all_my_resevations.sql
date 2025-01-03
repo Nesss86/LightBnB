ea4fee7b2246ac3e45343601cbf303b874660347
Nesss86 <Nessymonster86@gmail.com>
2025-01-03 17:18:49 -0500

    Created 1_queries.sql and added queries

---
+++ 1_queries/all_my_resevations.sql
@@ -0,0 +1,13 @@
+SELECT 
+    reservations.id AS id,
+    properties.title AS title,
+    properties.cost_per_night,
+    reservations.start_date,
+    AVG(property_reviews.rating) AS average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY reservations.id, properties.title, reservations.start_date, properties.cost_per_night
+ORDER BY reservations.start_date ASC
+LIMIT 10;
